060ca8d8845690949bad79c20cccdf6de4ef4266
Ana Menezes <anavick.sk@gmail.com>
2025-07-13 17:52:05 -0300

    Implements dependency injection

---
+++ UsersManager.Domain/Interfaces/IUsersService.cs
@@ -0,0 +1,13 @@
+﻿using UsersManager.Domain.Entities;
+
+namespace UsersManager.Domain.Interfaces
+{
+    public interface IUsersService
+    {
+        List<User> GetUsers();
+        User? GetUserById(Guid userId);
+        void AddUser(User newUser);
+        void UpdateUser(Guid id, User user);
+        void DeleteUser(Guid userId);
+    }
+}
